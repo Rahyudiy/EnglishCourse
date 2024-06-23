21c4079706b42a972dbdc3f58a119cb9660b7b75
Ryuuxy <gdengurahyudiantara@gmail.com>
2024-06-23 09:55:37 +0800

    progres 23/6/24 - Responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,22 @@
 const listNav = document.getElementById("navLink");
+const sectionPrice = document.getElementById("price");
+const priceTag = document.getElementById("priceTag");
 listNav.style.maxHeight = "0px";
 
-
 function toggleNavbar() {
-  if ((listNav.style.maxHeight === "0px")) {
+  if (listNav.style.maxHeight === "0px") {
     listNav.style.maxHeight = "400px";
   } else {
     listNav.style.maxHeight = "0px";
   }
 }
+
+//price zoom-in
+window.addEventListener("scroll", function () {
+  const pastPriceSection = sectionPrice.getBoundingClientRect().top;
+  if (pastPriceSection <= 0) {
+    priceTag.classList.add("price-zoom-in");
+  } else {
+    priceTag.classList.remove("price-zoom-in");
+  }
+});
